1ef7e9c301cd4b3212c09533fc85be9d5decf744
Simon Lee <simonlee0306@gmail.com>
2018-10-18 19:11:27 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,11 +6,11 @@ end
 
 def summon_captain_planet(planeteers)
   planeteers.collect {|x| x.capitalize << "!"}
-
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.include? {|x| x.length > 4}
+
 end
 
 def find_the_cheese# code an argument here
