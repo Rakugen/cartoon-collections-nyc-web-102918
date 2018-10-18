9ebb76a5f18552b775e85299628c5272708f525e
Simon Lee <simonlee0306@gmail.com>
2018-10-18 19:26:59 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,13 @@ def long_planeteer_calls(calls)
   calls.any? {|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(strings)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  if strings.include?(cheese_types)
+    true
+  else
+    false
+  end
 end
